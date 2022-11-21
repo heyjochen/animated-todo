6afa954a62df3f1ad3421d4ffd70d47157472455
Jochen Stierberger <jochen@stierberger.com>
2022-11-21 08:08:37 -0600

    create AppContainer

---
+++ src/screens/main.tsx
@@ -0,0 +1,16 @@
+import { Text, Box, Center } from 'native-base'
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={4}
+      flex={1}
+    >
+      <Box>
+        <Text>Hello</Text>
+      </Box>
+    </Center>
+  )
+}
